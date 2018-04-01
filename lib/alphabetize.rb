39ba8cb4027d49d2ab3c3fc58130114091cf1bb0
Shun Yao <ysmiracle@hotmail.com>
2018-04-01 00:17:38 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,10 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|ESPERANTO_ALPHABET| ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"}
+  
+  arr.sort_by {
+    |element| element.chars.each {
+      |char| ESPERANTO_ALPHABET.index(char)
+      }
+    }
+  }
 end
