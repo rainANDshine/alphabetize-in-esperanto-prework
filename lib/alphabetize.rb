6dcb940f9f4c246c2f621e94c30c8426e162a1cb
Shun Yao <ysmiracle@hotmail.com>
2018-04-01 00:19:09 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,9 @@
-ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-
 def alphabetize(arr)
-  
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
   arr.sort_by {
     |element| element.chars.each {
       |char| ESPERANTO_ALPHABET.index(char)
       }
     }
-  }
 end
